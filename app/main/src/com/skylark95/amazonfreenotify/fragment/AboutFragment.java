02340c058e1804eb995adf3894e84dc88b473a53
Derek <skylark95atc@gmail.com>
2012-11-29 00:11:55 -0600

    Reorg project structure.

---
+++ app/main/src/com/skylark95/amazonfreenotify/fragment/AboutFragment.java
@@ -0,0 +1,23 @@
+package com.skylark95.amazonfreenotify.fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.actionbarsherlock.app.SherlockFragment;
+import com.skylark95.amazonfreenotify.R;
+
+public class AboutFragment extends SherlockFragment {
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	}
+	
+	 @Override
+     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+		 return inflater.inflate(R.layout.about_tab, container, false);
+	 }
+
+}
