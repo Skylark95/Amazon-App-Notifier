7bfe78eb2b377d3f0ecc284c99bc0ac51ba1661d
Skylark95 <skylark95atc@gmail.com>
2013-03-17 00:52:57 -0500

    Refactor DAO and add test with mocks.

---
+++ php/src/main/php/com/skylark95/amazonnotify/classes/dao/AppDataDao.php
@@ -0,0 +1,84 @@
+<?php
+
+require_once 'classes/dao/DataAccessException.php';
+require_once 'classes/dao/DBConfig.php';
+
+class AppDataDao {
+	
+	private $mysqli;
+	
+	public function __construct($mysqli) {
+		$this->mysqli = $mysqli;
+	}
+	
+	public static function connect() {
+		$mysqliConn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+		if ($mysqliConn->connect_errno) {
+			$message = "Failed to connect to MySQL: " . $mysqliConn->connect_error;
+			throw new DataAccessException($message);
+		}
+		return $mysqliConn;
+	}
+	
+	public function insertTodaysAppData($appData) {		
+		$appUrl = $this->mysqli->real_escape_string($appData[APP_URL]);
+		$appTitle = $this->mysqli->real_escape_string($appData[APP_TITLE]);
+		$appDeveloper = $this->mysqli->real_escape_string($appData[APP_DEVELOPER]);
+		$appListPrice = $this->mysqli->real_escape_string($appData[APP_LIST_PRICE]);
+		$appCategory = $this->mysqli->real_escape_string($appData[APP_CATEGORY]);
+		$appDescription = $this->mysqli->real_escape_string($appData[APP_DESCRIPTION]);
+			
+		$result = $this->mysqli->query(
+			"INSERT INTO free_app_of_day (
+					app_date,
+					app_url,
+					app_title,
+					app_developer,
+					app_list_price,
+					app_category,
+					app_description)
+				VALUES (
+					CURDATE(),
+					'$appUrl',
+					'$appTitle',
+					'$appDeveloper',
+					'$appListPrice',
+					'$appCategory',
+					'$appDescription'
+					)");
+		
+		if(!$result) {
+			$message = 'AppDataDao: Failed to insert: (' . $this->mysqli->errno . ') ' . $this->mysqli->error;
+			throw new DataAccessException($message);
+		}
+	}
+	
+	public function selectAppDataForDate($date) {		
+		$result = $this->mysqli->query("SELECT
+								app_date,
+								app_url,
+								app_title,
+								app_developer,
+								app_list_price,
+								app_category,
+								app_description
+							  FROM free_app_of_day
+							  WHERE app_date = '$date'");
+		
+		if(!$result) {
+			$message = 'AppDataDao: Failed to select: (' . $this->mysqli->errno . ') ' . $this->mysqli->error;
+			throw new DataAccessException($message);			
+		}		
+		
+		if ($this->mysqli->affected_rows == 0) {
+			$message = "AppDataDao: No rows found for $date";
+			throw new DataAccessException($message);
+		}
+		
+		return $result->fetch_assoc();
+	}
+
+	
+}
+
+?>
---
+++ php/src/test/php/com/skylark95/amazonnotify/classes/dao/AppDataDaoTest.php
@@ -0,0 +1,162 @@
+<?php
+
+require_once 'classes/dao/AppDataDao.php';
+require_once 'classes/dao/DataAccessException.php';
+require_once 'config.php';
+
+class AppDataDaoTest extends PHPUnit_Framework_TestCase {
+	
+	/**
+	 * @test
+	 */
+	public function verifyInsertTodaysAppData() {		
+		$appData = $this->getAppData();
+		$appUrl = $appData[APP_URL];
+		$appTitle = $appData[APP_TITLE];
+		$appDeveloper = $appData[APP_DEVELOPER];
+		$appListPrice = $appData[APP_LIST_PRICE];
+		$appCategory = $appData[APP_CATEGORY];
+		$appDescription = $appData[APP_DESCRIPTION];
+		
+		$mockMysqli = $this->getMock('mysqli', array('query', 'real_escape_string'));
+	
+		$mockMysqli->expects($this->exactly(6))
+				->method('real_escape_string')
+				->will($this->returnArgument(0));
+		
+		$mockMysqli->expects($this->once())
+				->method('query')
+				->will($this->returnValue(true))
+				->with($this->equalTo("INSERT INTO free_app_of_day (
+					app_date,
+					app_url,
+					app_title,
+					app_developer,
+					app_list_price,
+					app_category,
+					app_description)
+				VALUES (
+					CURDATE(),
+					'$appUrl',
+					'$appTitle',
+					'$appDeveloper',
+					'$appListPrice',
+					'$appCategory',
+					'$appDescription'
+					)"));
+		
+		$appDataDao = new AppDataDao($mockMysqli);
+		$appDataDao->insertTodaysAppData($appData);
+	}
+	
+	/**
+	 * @test
+	 * @expectedException DataAccessException
+	 * @expectedExceptionMessage AppDataDao: Failed to insert: (0) Some Error Message
+	 */
+	public function throwDataAccessExceptionIfInsertTodaysAppDataFails() {
+		$appData = $this->getAppData();
+		$mockMysqli = $this->getMock('mysqli', array('query', 'real_escape_string'));
+		
+		$mockMysqli->expects($this->exactly(6))
+				->method('real_escape_string')
+				->will($this->returnArgument(0));
+		
+		$mockMysqli->expects($this->once())
+				->method('query')
+				->will($this->returnValue(false));
+		
+		$mockMysqli->errno = 0;
+		$mockMysqli->error = 'Some Error Message';
+		
+		$appDataDao = new AppDataDao($mockMysqli);
+		$appDataDao->insertTodaysAppData($appData);
+	}
+	
+	/**
+	 * @test
+	 */
+	public function verifySelectAppDataForDate() {
+		$mockMysqli = $this->getMock('mysqli', array('query'));
+		$mockMysqli_result = $this->getMock('mysqli_result', array('fetch_assoc'));
+		
+		$appData = $this->getAppData();
+		$mockMysqli_result->expects($this->once())
+				->method('fetch_assoc')
+				->will($this->returnValue($appData));
+		
+		$date = '2013-03-17';
+		$mockMysqli->expects($this->once())
+				->method('query')
+				->will($this->returnValue($mockMysqli_result))
+				->with($this->equalTo("SELECT
+								app_date,
+								app_url,
+								app_title,
+								app_developer,
+								app_list_price,
+								app_category,
+								app_description
+							  FROM free_app_of_day
+							  WHERE app_date = '$date'"));
+		
+		$mockMysqli->affected_rows = 1;
+		
+		$appDataDao = new AppDataDao($mockMysqli);
+		$returnedData = $appDataDao->selectAppDataForDate($date);
+		$this->assertEquals($appData, $returnedData);
+	}
+	
+	/**
+	 * @test
+	 * @expectedException DataAccessException
+	 * @expectedExceptionMessage AppDataDao: No rows found for 2013-03-17 
+	 */
+	public function selectAppDataForDateWhenNoRecordsThrowsException() {
+		$mockMysqli = $this->getMock('mysqli', array('query'));
+		
+		$date = '2013-03-17';
+		$mockMysqli->expects($this->once())
+				->method('query')
+				->will($this->returnValue(true));
+		
+		$mockMysqli->affected_rows = 0;
+		
+		$appDataDao = new AppDataDao($mockMysqli);
+		$appDataDao->selectAppDataForDate($date);
+	}
+	
+	/**
+	 * @test
+	 * @expectedException DataAccessException
+	 * @expectedExceptionMessage AppDataDao: Failed to select: (1) Another Error
+	 */
+	public function selectAppDataForDateWhenFailedThrowsException() {
+		$mockMysqli = $this->getMock('mysqli', array('query'));
+		
+		$date = '2013-03-17';
+		$mockMysqli->expects($this->once())
+				->method('query')
+				->will($this->returnValue(false));
+		
+		$mockMysqli->errno = 1;
+		$mockMysqli->error = 'Another Error';
+		
+		$appDataDao = new AppDataDao($mockMysqli);
+		$appDataDao->selectAppDataForDate($date);
+	}
+	
+	private function getAppData() {
+		$appData = array();
+		$appData[APP_URL] = 'http://www.amazon.com/gp/product/B008I3RARW/';
+		$appData[APP_TITLE] = 'Race Rally 3D - Racing Car Arcade Fun';
+		$appData[APP_DEVELOPER] = 'Sulaba Inc';
+		$appData[APP_LIST_PRICE] = '$4.99';
+		$appData[APP_CATEGORY] = 'Games';
+		$appData[APP_DESCRIPTION] = 'The Race Rally is the best 3d arcade racing game';
+		return $appData;
+	}
+	
+}
+
+?>
