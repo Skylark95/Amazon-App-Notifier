99eb0a1ef3187c091f9ee4033ed06e96e1a675d1
Skylark95 <derek@skylark95.com>
2013-04-06 23:48:13 -0500

    Fix for web host not supporting nested excptions.

--- php/src/main/php/com/skylark95/amazonnotify/classes/dao/DataAccessException.php
+++ php/src/main/php/com/skylark95/amazonnotify/classes/dao/DataAccessException.php
@@ -2,8 +2,8 @@
 
 class DataAccessException extends Exception {
 	
-	public function __construct($message, $code = 0, Exception $previous = null) {
-		parent::__construct($message, $code, $previous);
+	public function __construct($message, $code = 0) {
+		parent::__construct($message, $code);
 	}
 
 }
