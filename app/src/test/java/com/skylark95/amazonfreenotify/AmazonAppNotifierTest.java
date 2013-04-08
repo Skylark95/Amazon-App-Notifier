028701d1a55f0950f6c9754160fec86400db2339
Skylark95 <derek@skylark95.com>
2013-04-07 23:19:37 -0500

    App Release 2.0-BETA

--- app/src/test/java/com/skylark95/amazonfreenotify/AmazonAppNotifierTest.java
+++ app/src/test/java/com/skylark95/amazonfreenotify/AmazonAppNotifierTest.java
@@ -1,3 +1,24 @@
+/*
+ * This file is part of Amazon App Notifier
+ *
+ * Copyright 2013 Derek <derek@skylark95.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Amazon App Notifier is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Amazon App Notifier. if not, If not, see <http://www.gnu.org/licenses/>.
+ *
+ *
+ */
+
 package com.skylark95.amazonfreenotify;
 
 import static com.xtremelabs.robolectric.Robolectric.*;
@@ -37,7 +58,6 @@ public class AmazonAppNotifierTest {
 	
 	private static final String IS_FIRST_START = "_is_first_start";
 	private static final String TAB_POSITION = "tab_position";
-	private static final String CHANGELOG_KEY = "show_changelog";
 
 	private static class AmazonAppNotifierTestWrapper extends AmazonAppNotifier {
 		
--- app/src/test/java/com/skylark95/amazonfreenotify/service/FreeAppNotificationServiceTest.java
+++ app/src/test/java/com/skylark95/amazonfreenotify/service/FreeAppNotificationServiceTest.java
@@ -1,24 +1,42 @@
+/*
+ * This file is part of Amazon App Notifier
+ *
+ * Copyright 2013 Derek <derek@skylark95.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Amazon App Notifier is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Amazon App Notifier. if not, If not, see <http://www.gnu.org/licenses/>.
+ *
+ *
+ */
+
 package com.skylark95.amazonfreenotify.service;
 
 import static com.xtremelabs.robolectric.Robolectric.*;
 import static org.junit.Assert.*;
 
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-
 import android.app.Notification;
 import android.app.NotificationManager;
 import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
-import android.preference.PreferenceManager;
 
 import com.actionbarsherlock.app.SherlockFragmentActivity;
-import com.skylark95.amazonfreenotify.settings.PrefNotificationDays;
 import com.skylark95.amazonfreenotify.util.TestUtils;
 import com.xtremelabs.robolectric.RobolectricTestRunner;
 import com.xtremelabs.robolectric.shadows.ShadowNotificationManager;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 
 @RunWith(RobolectricTestRunner.class)
 public class FreeAppNotificationServiceTest {
