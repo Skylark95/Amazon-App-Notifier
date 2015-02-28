c3bd0ce0e682000a8dd62928717e39a18b7a4134
Derek <derek@skylark95.com>
2015-02-27 21:37:56 -0600

    icon

--- FreeAppNotifier/app/src/test/java/com/skylark95/amazonfreenotify/TestConfig.java
+++ FreeAppNotifier/app/src/test/java/com/skylark95/amazonfreenotify/TestConfig.java
@@ -2,6 +2,6 @@ package com.skylark95.amazonfreenotify;
 
 public class TestConfig {
 
-    public static final String MANIFEST = "app/src/main/AndroidManifest.xml";
+    public static final String MANIFEST = "src/main/AndroidManifest.xml";
     public static final int EMULATE_SDK = 18;
 }
